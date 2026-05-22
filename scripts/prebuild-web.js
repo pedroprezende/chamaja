5ae13da42713445ac622ecbf4dd15c2acb36cc64
Pedro <pedro.mlk100@gmail.com>
2026-05-22 19:10:07 -0300

    chore: use CommonJS require in prebuild-web.js to eliminate build warning

--- scripts/prebuild-web.js
+++ scripts/prebuild-web.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 const dir = path.join("node_modules", "react-native-css-interop", ".cache");
 const file = path.join(dir, "web.css");
