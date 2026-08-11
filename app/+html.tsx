1df32e8cc2b34570f9697b2906efb8ddd9fa2a42
Pedro <pedro.mlk100@gmail.com>
2026-08-11 17:55:54 -0300

    feat: adicionar Google Tag Manager (GTM-W9RMVT7L)

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,6 +9,18 @@ export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W9RMVT7L');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
@@ -54,7 +66,19 @@ export default function Root({ children }: PropsWithChildren) {
 
         {/* CSS Global e fontes podem ir aqui */}
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W9RMVT7L"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   );
 }
